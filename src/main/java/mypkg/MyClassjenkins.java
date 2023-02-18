49c77e7ee6b2a13a89569a609c29dfec57130cf1
Avika Agarwal <avikaagarwal2@gmail.com>
2023-02-18 23:51:42 +0530

    first commit

---
+++ src/main/java/mypkg/MyClassjenkins.java
@@ -0,0 +1,16 @@
+package mypkg;
+
+public class MyClassjenkins {
+	public void show()
+	{
+		System.out.println("Hello people! Welcome to jenkins");
+	}
+
+	public static void main(String[] args) {
+		MyClassjenkins ob = new MyClassjenkins();
+		ob.show();
+		
+
+	}
+
+}
